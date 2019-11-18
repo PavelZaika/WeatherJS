18a8f5595b0ac314b7f724bd53937e681b9755ca
Loki <zpk.loki@gmail.com>
2019-11-18 15:07:42 +0300

    ui

--- ui.js
+++ ui.js
@@ -18,10 +18,17 @@ class UI {
       
 
       this.location.textContent = `${weather.name}, ${weather.sys.country}`;
-      this.desc.textContent = weather.weather.main;
+      this.desc.textContent = weather.weather[0].main;
       this.pressure.textContent = `${weather.main.pressure} hPa`;
       this.string.textContent = `${weather.main.temp} Celsius`;
-      this.icon.setAttribute('src', `http://openweathermap.org/img/wn/${weather.weather.icon}@2x.png`);
+
+      if (weather.weather.icon != 'undefined') {
+        this.icon.setAttribute('src', `http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`);
+      } else {
+        console.log('error')
+      }
+
+      
       this.humidity.textContent = `Relative Humidity: ${weather.main.humidity} %`;
       this.tempMin.textContent = `Minimum temperature at the moment: ${weather.main.temp_min} Celsius`;
       this.tempMax.textContent = `Maximum temperature at the moment: ${weather.main.temp_max} Celsius`;
